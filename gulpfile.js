b248261eafbfbcfa6924a6c78dcb2a0d725a3a6d
ellen <ellen.su@afusion.com>
2018-03-08 23:17:24 +0800

    change to docker hub

--- gulpfile.js
+++ gulpfile.js
@@ -11,24 +11,24 @@ gulp.task('NpmDeploy', function(){
 })
 
 gulp.task('BuildDockerImage',['NpmBuild'],function(){
-	exec('docker build -f Dockerfile -t localhost:5000/nginxweb:1.0 -t ocalhost:5000/nginxweb:latest --rm --no-cache .', function (err,outlog,errlog){
+	exec('docker build -f Dockerfile -t ellensu/nginxweb:' + args.buildversion +' -t ellensu/nginxweb:latest --rm --no-cache .', function (err,outlog,errlog){
 		console.log(outlog);
 		console.log(errlog);
 	});
 })
 
 gulp.task('PushImage',['BuildDockerImage'],function(){
-	exec('docker push localhost:5000/nginxweb:' + args.buildversion, function (err,outlog,errlog){
+	exec('docker push ellensu/nginxweb:' + args.buildversion, function (err,outlog,errlog){
 		console.log(outlog);
 		console.log(errlog);
 	});
-	exec('docker push localhost:5000/nginxweb:latest', function (err,outlog,errlog){
+	exec('docker push ellensu/nginxweb:latest', function (err,outlog,errlog){
 		console.log(outlog);
 		console.log(errlog);
 	});
 })
 //這段是for play-with-docker特別寫的 與實際server ssh不同
-gulp.task('UpdateDockerCompose',['DeleteImage'],function() {
+gulp.task('UpdateDockerCompose',['PushImage'],function() {
 	exec('cd /docker/');
 	exec('ssh ip172-18-0-16-baga5moo6i4000c49q6g@direct.labs.play-with-docker.com');
 	exec('docker-compose pull web','docker-compose up -d --no-deps --build web');
